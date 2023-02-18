36a5b56862ac5ef8a25a6b9b11bd2ada207d653f
Erbol Betenov <betenov.e98@gmail.com>
2023-02-18 23:49:07 +0600

    commit 4

--- app.ts
+++ app.ts
@@ -1,19 +1,30 @@
 const baseUrl = 'http://146.185.154.90:8000/blog/leo@mail.ru/';
 
-const  userName = document.getElementById('userName')!;
+const userName = document.getElementById('userName')!;
 const editProfileBtn = document.getElementById('editProfile')!
 const profileModal = new bootstrap.Modal('#exampleModal');
-const firstNameInput = <HTMLInputElement> document.getElementById('firstName')!;
-const lastNameInput = <HTMLInputElement> document.getElementById('lastName')!;
-const profileForm = <HTMLFormElement>document.getElementById('profileForm');
-
+const firstNameInput = <HTMLInputElement>document.getElementById('firstName');
+const lastNameInput = <HTMLInputElement>document.getElementById('lastName');
+const profileForm = document.getElementById('profileForm')!;
+const messages = document.getElementById('messages')!;
+const messageForm = document.getElementById('messageForm')!;
+const messageInput = <HTMLInputElement>document.getElementById('message');
 
 interface ProfileResponse {
-  email:string;
-  firstName:string;
-  lastName:string;
-  _id:string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  _id: string;
 }
+
+interface Post {
+  datetime: string;
+  message: string;
+  user: ProfileResponse;
+  userId: string;
+  _id: string;
+}
+
 const run = async () => {
   const response = await fetch(baseUrl + 'profile');
   const user: ProfileResponse = await response.json();
@@ -34,7 +45,7 @@ const run = async () => {
     body.append('firstName', firstName);
     body.append('lastName', lastName);
     try {
-      await fetch(baseUrl + 'profile', {method:'POST', body});
+      await fetch(baseUrl + 'profile', {method: 'POST', body});
       profileModal.hide();
       userName.innerHTML = firstName + ' ' + lastName;
       user.firstName = firstName;
@@ -43,6 +54,44 @@ const run = async () => {
       alert('Error' + e)
     }
   });
+
+  messageForm.addEventListener('submit', async event => {
+    event.preventDefault();
+    const message = messageInput.value;
+
+    const body = new URLSearchParams();
+    body.append('message', message);
+
+    try {
+      await fetch(baseUrl + 'posts', {method: 'POST', body})
+    } catch (e) {
+      alert('Error' + e)
+    }
+  })
+
+  let lastDate = '';
+
+  setInterval(async () => {
+    let url = baseUrl + 'posts';
+    if (lastDate) {
+      url += '?datetime=' + lastDate
+    }
+    const response = await fetch(url);
+    const posts: Post[] = await response.json();
+    if (posts.length  > 0){
+      lastDate = posts[posts.length-1].datetime
+    }
+    for (const post of posts) {
+      const message = document.createElement('div');
+      message.innerHTML = `
+      <strong>${post.user.firstName + ' ' + post.user.lastName} said:<strong/>
+      <blockquote>${post.message}<blockquote/>
+      `;
+      message.className = 'card card-body mb-2';
+      messages.insertBefore(message, messages.firstElementChild)
+    }
+
+  }, 3000)
 };
 
 run().catch(console.error)
